abae6f804794d4270a277a7dbac06134bb23fc7a
James Katarikawe <james.katarikawe@andela.com>
2019-11-19 20:46:43 +0300

    Remove routes and controller files

--- src/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-Route::get('districts', function(){
-	echo 'This should return all districts';
-});
-
-Route::get('regions/', 'JaymesKat\UgandaGeography\RegionsController@all');
-
-Route::get('regions/{id}', 'JaymesKat\UgandaGeography\RegionsController@single');
